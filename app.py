8d0cf4415b58698f4f3989e0af4180ff6b11e3d5
famnindonindo <famnathakit2@gmail.com>
2024-09-16 18:30:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ if "messages" not in st.session_state:
         }
     ]
 
-file_path = "insurverse_p1 (19).xlsx"
+file_path = "insurverse_p1 (21).xlsx"
 try:
     df = pd.read_excel(file_path)
     file_content = df.to_string(index=False)
